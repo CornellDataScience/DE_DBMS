99de1e84e83c7f0d0e5203a7e082dd87f56359a4
Anoop Balakrishnan <banoop@gmail.com>
2018-11-11 13:01:04 -0500

    Set up join operator

---
+++ src/operator/Join.java
@@ -0,0 +1,66 @@
+package operator;
+
+import DBSystem.Table;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import DBSystem.Column;
+import DBSystem.Tuple;
+import DBSystem.dbCatalog;
+
+public class Join extends Operator {
+
+	/**
+	 * Join Operator is called when JOIN keyword is called in the SQL Language. 
+	 */
+
+	List<Table> tables;
+	List<Table> origTables;
+	String tableName;
+	List<String> colNames;
+
+
+
+	public Join(List<String> tableNames, List<String> colNames) {
+		tables = new ArrayList<Table>();
+		origTables = new ArrayList<Table>();
+		for(String s: tableNames) {
+			tables.add(dbCatalog.getTable(s));
+		}
+		origTables = tables;
+		this.colNames = colNames;
+
+	}
+
+	@Override
+	public Table operate() {
+		//Converting list of column names to columns 
+		List<Column> cols = new ArrayList<Column>();
+		for(int i = 0; i < tables.size(); i ++) {
+			cols.add(tables.get(i).getCol(colNames.get(i)));
+		}
+		for(int i = 0; i < tables.size(); i ++) {
+			HashMap<Integer,elem> map = new HashMap<Integer, elem>();
+			for(int j = 0; j < cols.get(i).getSize(); j ++) {
+				//Put hash mapped to elem instance of object and its index
+			}
+		}
+		return new Table(tableName, cols);
+	}
+
+	@Override
+	public void reset() {
+		tables = origTables;
+	}
+	private class elem {
+		private Object o;
+		private int index;
+		
+		public elem(Object o, int index) {
+			this.o = o;
+			this.index = index;
+		}
+	}
+}
