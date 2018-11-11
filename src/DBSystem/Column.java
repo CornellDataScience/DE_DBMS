75c0214d11bfc546fbcb520bbaca9b2163628ca2
EageRAssassin <rt389@cornell.edu>
2018-11-11 12:15:41 -0500

    duplicate elimination

--- src/DBSystem/Column.java
+++ src/DBSystem/Column.java
@@ -21,6 +21,40 @@ public class Column {
 	}
 	
 	/**
+	 * Method to get size of the column
+	 * 
+	 * @return the size of the Column
+	 */
+	public int getSize() {
+		return data.size();
+	}
+	
+	/**
+	 * Method to get nth data in the column
+	 * 
+	 * @return the nth data in the column
+	 */
+	public Object getData(int n) {
+		return data.get(n);
+	}
+	
+	/**
+	 * Method to add data to the column
+	 * 
+	 */
+	public void addData(Object o) {
+		data.add(o);
+	}
+	
+	/**
+	 * Method to clear all the data in the column
+	 * 
+	 */
+	public void resetColumn() {
+		data = new ArrayList<Object>();
+	}
+	
+	/**
 	 * Method to convert the Column class to String
 	 * 
 	 * @return the column's name and data
@@ -29,4 +63,5 @@ public class Column {
 		String output = name + "\n" + data;
 		return output;
 	}
+	
 }
